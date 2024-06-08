c7b347dcf8b5387bc5ac19f09501170206b4085f
jigneshbambhava <154456886+jigneshbambhava@users.noreply.github.com>
2024-06-08 13:27:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import Header from './components/Header';
 import BlogPage from './components/BlogPage';  // Import BlogPage component
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
     <div>
       <Header/>
       <BlogPage/>
+      <Footer/>
     </div>
   );
 };
