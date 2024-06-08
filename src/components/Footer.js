12d91046e3cf65ebb02e1cb3804fea120ea8e715
jigneshbambhava <154456886+jigneshbambhava@users.noreply.github.com>
2024-06-08 13:26:26 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Footer.css'
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>Created by Jignesh</p>
+        <p>&copy; {currentYear} . All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
